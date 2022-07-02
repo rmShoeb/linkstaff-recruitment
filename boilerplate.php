50452771e6f2a33fb8beb01d65603da45f8a4f82
rmShoeb <rmshoebne14@gmail.com>
2022-07-03 00:03:25 +0600

    minor changes

--- boilerplate.php
+++ boilerplate.php
@@ -2,14 +2,12 @@
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <link rel = "icon" href ="https://img.icons8.com/ios/50/000000/apple-calculator.png" type = "image/x-icon">
+    <title>Calculator</title>
 </head>
 
 <body>
