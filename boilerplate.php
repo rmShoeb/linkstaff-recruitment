4cbd00583c7552becbe7526c5501176c1e6b2d10
rmShoeb <rmshoebne14@gmail.com>
2022-07-02 23:54:12 +0600

    added boilerplate

---
+++ boilerplate.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <form action="#" method="post" class="row m-5">
+        <div class="col input-group m-1">
+            <span class="input-group-text" id="basic-addon1">Operand 1</span>
+            <input type="text" class="form-control" placeholder="Should be an integer">
+        </div>
+        <div class="col m-1">
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Operator</option>
+                <option value="1">&#x1F47D</option>
+                <option value="2">&#x1F480</option>
+                <option value="3">&#x1F47B</option>
+                <option value="4">&#x1F631</option>
+            </select>
+        </div>
+        <div class="col input-group m-1">
+            <span class="input-group-text" id="basic-addon1">Operand 2</span>
+            <input type="text" class="form-control" placeholder="Should be an integer">
+        </div>
+        <div class="col">
+            <button type="submit" class="btn btn-primary m-1">Calculate</button>
+        </div>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
